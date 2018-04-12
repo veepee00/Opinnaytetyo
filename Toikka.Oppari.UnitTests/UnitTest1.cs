6693c374962bfa3f6e74555a887b20f62886bef2
veepee00 <35451981+veepee00@users.noreply.github.com>
2018-04-12 13:28:14 +0300

    CheckOldFilesFromDirectory

--- Toikka.Oppari.UnitTests/UnitTest1.cs
+++ Toikka.Oppari.UnitTests/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace Toikka.Oppari.UnitTests
 {
@@ -13,9 +14,9 @@ namespace Toikka.Oppari.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
-            var files = CheckOldFilesFromDirectory(@"C:\Temp", "*.txt");
+            var files = CheckOldFilesFromDirectory(@"C:\Temp", ".txt");
 
-            Assert.IsTrue(files.Count == 2);
+            Assert.IsTrue(files.Count == 3);
         }
 
         [TestMethod]
@@ -27,9 +28,31 @@ namespace Toikka.Oppari.UnitTests
             Assert.ThrowsException<ArgumentNullException>(() => CheckOldFilesFromDirectory("", "*."));
         }
 
-        private List<String> CheckOldFilesFromDirectory(string folder, string mask)
-        {
-            throw new NotImplementedException();
+        int time = -10;
+        private List<string> CheckOldFilesFromDirectory(string folder, string mask)
+        {          
+            if (folder == "" || folder == null)
+            {
+                throw new ArgumentNullException();              
+            }
+            if (mask == "" || mask == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            List<string> files = new List<string>(Directory.GetFiles(folder));
+            List<string> returnFiles = new List<string>();
+
+            foreach (string file in files)
+            {
+                FileInfo fi = new FileInfo(file);
+                if ((fi.LastAccessTime < DateTime.Now.AddMinutes(time)) && fi.Extension == mask)
+                {
+                    Console.WriteLine(fi.Extension);
+                    returnFiles.Add(file);
+                }
+            }
+            return returnFiles;
         }
     }
 }
