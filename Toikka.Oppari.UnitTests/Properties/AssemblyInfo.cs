802b591132c042a05c1f609021b0fd00c6d5d1d5
veepee00 <35451981+veepee00@users.noreply.github.com>
2018-04-12 12:12:33 +0300

    Ohjelma

---
+++ Toikka.Oppari.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Toikka.Oppari.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Toikka.Oppari.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("debdf795-0a61-4c7e-8d51-8b0ffcfdb844")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Toikka.Oppari.UnitTests/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Toikka.Oppari.UnitTests
+{
+
+    
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var files = CheckOldFilesFromDirectory(@"C:\Temp", "*.txt");
+
+            Assert.IsTrue(files.Count == 2);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => CheckOldFilesFromDirectory(@"C:\Temp", null));
+            Assert.ThrowsException<ArgumentNullException>(() => CheckOldFilesFromDirectory(null, "*."));
+            Assert.ThrowsException<ArgumentNullException>(() => CheckOldFilesFromDirectory(@"C:\Temp", ""));
+            Assert.ThrowsException<ArgumentNullException>(() => CheckOldFilesFromDirectory("", "*."));
+        }
+
+        private List<String> CheckOldFilesFromDirectory(string folder, string mask)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
